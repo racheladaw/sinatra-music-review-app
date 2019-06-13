79d2c57f06346949dd976bc287a1c561c0f7a38b
Rachel <williamsrachelada@gmail.com>
2019-06-12 20:04:50 -0700

    Added more user routes and logout feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+
+    def logged_in?(session)
+      !!session[:user_id]
+    end
+
+    def current_user(session)
+      User.find(session[:user_id])
+    end 
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,51 @@ class UsersController < ApplicationController
   post '/users' do
     #binding.pry
     u = User.new
-    u.username = params[:username]
-    u.password = params[:password]
-    u.save
+    if !params[:username].empty?
+      User.all.each do |user|
+        if user.username == params[:username]
+          redirect '/users/new'
+        end
+      end
+      u.username = params[:username]
+      u.password = params[:password]
+      if u.save
+        redirect '/login'
+      end
+    else
+      redirect '/users/new'
+    end
   end
 
   get '/users/:id' do
     @user = User.find(params[:id])
     erb :'/users/show'
-  end 
+  end
+
+  get '/login' do
+    #binding.pry
+    if !logged_in?(session)
+      erb :'/users/login'
+    else
+      redirect "/users/#{current_user(session).id}"
+    end
+  end
+
+  post '/login' do
+    #binding.pry
+    u = User.find_by(username: params[:username])
+    if u && u.authenticate(params[:password])
+      session[:user_id] = u.id
+      redirect "/users/#{u.id}"
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+  end
+
+
 
 end
