bb70b95470fbebd560a1820c9762f2de96c2a272
Rachel <williamsrachelada@gmail.com>
2019-06-15 18:11:21 -0700

    Fixed layout on some views for readability and added feature to view users of the app and link to their pages

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,17 +10,8 @@ class AlbumsController < ApplicationController
   end
 
   post '/albums' do
-    #binding.pry
     a = Album.new(name: params[:name], artist: params[:artist], release_date: params[:release_date])
-    # Album.all.each do |album|
-    #   if params[:name] == album.name
-    #     redirect '/albums/new'
-    #   end
-    # end
-    # r = Review.new(rating: params[:rating], content: params[:content])
     if a.save
-    # current_user(session).reviews << r
-    # a.reviews << r
       redirect '/albums'
     else
       redirect '/albums/new'
